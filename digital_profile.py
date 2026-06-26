c4d8c02efb34321d98ca25c15bd0ecf4997e3a1d
406-38-sippawich <54743@yupparaj.ac.th>
2026-06-26 10:32:47 +0700

    Create digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.title('My Digital Profile')
+
+st.header('แนะนำตัว')
+st.write('นายสิปปวิชญ์ พิบูลย์')
+st.write('ชื่อเล่น หนม')
+st.write('เลขที่ 38 ม.4/6')
+
+st.header('เป้าหมายและสิ่งที่อยากทำในอนาคต')
+st.write('เป้าหมาย: อยากเรียนเขียน Program เก่งๆ')
+st.write('สิ่งที่อยากเป็นในอนาคต: เป็น Programer และ Youtuber')
