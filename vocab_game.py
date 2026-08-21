ce7b76865b935f58c58801ffd9ef222ee7f3f044
406-38-sippawich <54743@yupparaj.ac.th>
2026-08-21 10:59:18 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -108,7 +108,7 @@ ans3 = st.text_input(
     value=st.session_state.ans3_val,
 )
 ans4 = st.text_input(
-    "ข้อ 4: 'w_t__m_lon' is full of water!. 🍉",
+    "ข้อ 4: `w_t__m_lon` is a fruit full of water!. 🍉",
     value=st.session_state.ans4_val,
 )
 
