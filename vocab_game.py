9bf2f0c78d0a590750128dacd9368c7ddea2bda0
406-38-sippawich <54743@yupparaj.ac.th>
2026-08-21 10:57:42 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -28,7 +28,7 @@ def reset_game():
 # 📌 ฟังก์ชัน MessageBox (Dialog)
 # ----------------------------------------------------
 @st.dialog("📊 สรุปผลการเล่นเกม")
-def show_result_dialog(ans1, ans2):
+def show_result_dialog(ans1, ans2, ans3, ans4):
     st.balloons()
     score = 0
 
@@ -59,7 +59,7 @@ def show_result_dialog(ans1, ans2):
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
     #ตรวจข้อ 4
-    if u_ans2 == "watermelon":
+    if u_ans4 == "watermelon":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
@@ -115,8 +115,8 @@ ans4 = st.text_input(
 # อัปเดตค่าล่าสุดเข้าตัวแปร
 st.session_state.ans1_val = ans1
 st.session_state.ans2_val = ans2
-st.session_state.ans1_val = ans3
-st.session_state.ans2_val = ans4
+st.session_state.ans3_val = ans3
+st.session_state.ans4_val = ans4
 
 # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มข้อ 3, 4 ตรงนี้
 
