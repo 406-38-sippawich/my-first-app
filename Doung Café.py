ff4c2565e97e6cb5e41000547f263f0146ed6689
406-38-sippawich <54743@yupparaj.ac.th>
2026-09-11 10:25:51 +0700

    Update Doung Café.py

--- Doung Café.py
+++ Doung Café.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.markdown("# :green[💪 Doung Café 🏋️]")
 st.write("ยินดีต้อนรับเข้าสู่ :green[Doung Café] กรุณาเลือกสินค้าของคุณ")
 st.write("Welcome to :green[Doung Café] Please select your order")
+st.divider()
