317c49bd74db216b47d5439c3b6af7c640431ade
406-38-sippawich <54743@yupparaj.ac.th>
2026-09-12 07:40:24 +0700

    Update Doung Café.py

--- Doung Café.py
+++ Doung Café.py
@@ -30,5 +30,6 @@ elif Cheesecake_count < 0:
   st.error(':red[Error จำนวนสินค้าไม่สามารถติดลบได้]')
 else:
   st.write(f'จำนวนสินค้า {count_sum} ชิ้น')
+st.text.input
 
 #st.write("Welcome to :green[Doung Café] Please select your order")
