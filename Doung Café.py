fc08c4c471e73a9789f2dd32fd856937e848d20d
406-38-sippawich <54743@yupparaj.ac.th>
2026-09-12 07:34:16 +0700

    Update Doung Café.py

--- Doung Café.py
+++ Doung Café.py
@@ -9,10 +9,26 @@ Latte_count = st.number_input('Latte ลาเต้ (แก้ว)' ,value = 0)
 Cappuccino_count = st.number_input('Cappuccino คาปูชิโน (แก้ว)' ,value = 0)
 Mocha_count = st.number_input('Mocha มอคค่า (แก้ว)' ,value = 0)
 IcedCoffee_count = st.number_input('Iced Coffee กาแฟเย็น (แก้ว)' ,value = 0)
-count_sum = IcedCoffee_count + Mocha_count + Cappuccino_count + Latte_count + Americano_count + Espresso_count
-if Espresso_count or Americano_count or Latte_count or Cappuccino_count or Mocha_count or IcedCoffee_count > -1 :
-  st.write(f'จำนวนสินค้า {count_sum} ชิ้น')
-else:
+Croissant_count = st.number_input('Croissant ครัวซองต์ (ชิ้น)' ,value = 0)
+Cheesecake_count = st.number_input('Cheesecake ชีสเค้ก (ชิ้น)' ,value = 0)
+count_sum = IcedCoffee_count + Mocha_count + Cappuccino_count + Latte_count + Americano_count + Espresso_count + Croissant_count + Cheesecake_count
+if Espresso_count < 0 :
   st.error(':red[Error จำนวนสินค้าไม่สามารถติดลบได้]')
+elif Americano_count < 0 :
+  st.error(':red[Error จำนวนสินค้าไม่สามารถติดลบได้]')
+elif Latte_count < 0:
+  st.error(':red[Error จำนวนสินค้าไม่สามารถติดลบได้]')
+elif Cappuccino_count < 0:
+  st.error(':red[Error จำนวนสินค้าไม่สามารถติดลบได้]')
+elif Mocha_count < 0:
+  st.error(':red[Error จำนวนสินค้าไม่สามารถติดลบได้]')
+elif IcedCoffee_count < 0:
+  st.error(':red[Error จำนวนสินค้าไม่สามารถติดลบได้]')
+elif Croissant_count < 0:
+  st.error(':red[Error จำนวนสินค้าไม่สามารถติดลบได้]')
+elif Cheesecake_count < 0:
+  st.error(':red[Error จำนวนสินค้าไม่สามารถติดลบได้]')
+else:
+  st.write(f'จำนวนสินค้า {count_sum} ชิ้น')
 
 #st.write("Welcome to :green[Doung Café] Please select your order")
