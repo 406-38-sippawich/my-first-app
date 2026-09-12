d73e8d8ec527db8a416111c073115422fd7d87de
406-38-sippawich <54743@yupparaj.ac.th>
2026-09-12 08:37:17 +0700

    Update Doung Café.py

--- Doung Café.py
+++ Doung Café.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.markdown("# :green[☕ Doung Café ]")
+st.markdown("# ☕:green[Doung Café ]")
 st.write("ยินดีต้อนรับเข้าสู่ ☕:green[Doung Café] กรุณาเลือกสินค้าของคุณ")
 st.write("Welcome to ☕:green[Doung Café] Please select your order")
 Espresso_count = st.number_input('☕Espresso เอสเพรสโซ (แก้ว)' ,value = 0)
