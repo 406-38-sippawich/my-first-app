67af8215376a4b93f91af5eb2baa0b7e5e0f2e5b
406-38-sippawich <54743@yupparaj.ac.th>
2026-09-12 07:49:00 +0700

    Update Doung Café.py

--- Doung Café.py
+++ Doung Café.py
@@ -30,6 +30,6 @@ elif Cheesecake_count < 0:
   st.error(':red[Error จำนวนสินค้าไม่สามารถติดลบได้]')
 else:
   st.write(f'จำนวนสินค้า {count_sum} ชิ้น')
+st.divider()
 membership = st.text_input("Do you have a membership? คุณได้สมัครสามาชิกไหม? (Normal/Gold/Platinum(ไม่มีให้เว้นว่าง))" , value= " " , )
-st.write(f'{membership}')
 
