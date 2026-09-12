b49dcb89abba88db914a609e398a6b517f3f6c35
406-38-sippawich <54743@yupparaj.ac.th>
2026-09-12 08:39:04 +0700

    Update Doung Café.py

--- Doung Café.py
+++ Doung Café.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
-st.markdown("# ☕:green[Doung Café ]")
-st.write("ยินดีต้อนรับเข้าสู่ ☕:green[Doung Café] กรุณาเลือกสินค้าของคุณ")
-st.write("Welcome to ☕:green[Doung Café] Please select your order")
-Espresso_count = st.number_input('☕Espresso เอสเพรสโซ (แก้ว)' ,value = 0)
-Americano_count = st.number_input('☕Americano อเมริกาโน (แก้ว)' ,value = 0)
-Latte_count = st.number_input('☕Latte ลาเต้ (แก้ว)' ,value = 0)
-Cappuccino_count = st.number_input('☕Cappuccino คาปูชิโน (แก้ว)' ,value = 0)
-Mocha_count = st.number_input('☕Mocha มอคค่า (แก้ว)' ,value = 0)
-IcedCoffee_count = st.number_input('☕Iced Coffee กาแฟเย็น (แก้ว)' ,value = 0)
+st.markdown("# :green[Doung Café ]")
+st.write("ยินดีต้อนรับเข้าสู่ :green[Doung Café] กรุณาเลือกสินค้าของคุณ")
+st.write("Welcome to :green[Doung Café] Please select your order")
+Espresso_count = st.number_input('Espresso เอสเพรสโซ (แก้ว)' ,value = 0)
+Americano_count = st.number_input('Americano อเมริกาโน (แก้ว)' ,value = 0)
+Latte_count = st.number_input('Latte ลาเต้ (แก้ว)' ,value = 0)
+Cappuccino_count = st.number_input('Cappuccino คาปูชิโน (แก้ว)' ,value = 0)
+Mocha_count = st.number_input('Mocha มอคค่า (แก้ว)' ,value = 0)
+IcedCoffee_count = st.number_input('Iced Coffee กาแฟเย็น (แก้ว)' ,value = 0)
 Croissant_count = st.number_input('🥐Croissant ครัวซองต์ (ชิ้น)' ,value = 0)
 Cheesecake_count = st.number_input('🍰Cheesecake ชีสเค้ก (ชิ้น)' ,value = 0)
 count_sum = IcedCoffee_count + Mocha_count + Cappuccino_count + Latte_count + Americano_count + Espresso_count + Croissant_count + Cheesecake_count
